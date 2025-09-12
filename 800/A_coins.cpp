a5461fd12ed3aeae53e769dc2339dce24a82c8c8
h11924 <11924harshitha@gmail.com>
2025-09-12 11:41:39 +0530

    A_coins.cpp

---
+++ 800/A_coins.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+using namespace std;        
+
+int main() {
+
+    
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        long long  n;
+        cin>>n ;
+
+        long long  k;
+        cin>>k;
+
+        if(n%2==0 || (n-k)%2==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+
+
+
+        
+
+       
+        
+
+       
+
+
+
+    }
+
+
+
+    
+
+
+    
+}
+
